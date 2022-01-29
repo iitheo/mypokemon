a1e73206e77152d044805b932f1889d4513e76dd
Theodore Kamma <iitheo@yahoo.com>
2022-01-30 00:04:10 +0100

    add initial project structure setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iitheo/theopokemon
 
 go 1.16
+
+require github.com/gomodule/redigo v1.8.8 // indirect
---
+++ pkg/app/config/dbconfigs/dbconfigs.go
@@ -0,0 +1,13 @@
+package dbconfigs
+
+import (
+	"github.com/gomodule/redigo/redis"
+	"os"
+)
+
+const REDISCONNSTRING = "redis://:p557474730a5035e5e552638b9c81aaecb57869b3bc71b0bacd1fc14f95fb551b@ec2-52-16-22-73.eu-west-1.compute.amazonaws.com:19519"
+
+func RedisConn() (redis.Conn, error) {
+	c, err := redis.DialURL(os.Getenv(REDISCONNSTRING), redis.DialTLSSkipVerify(true))
+	return c, err
+}
